5e169feffd58cf8ce5463aa183818f7c0ca50e68
rk-shikchi <ravikiran.shikchi@cognizant.com>
2017-01-20 12:26:24 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,9 @@
+
+public class add {
+public static int main1()
+{
+	int a=5,b=6;
+	System.out.println(a+b);
+	return a+b;
+}
+}
---
+++ addTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class addTest {
+
+	@Test
+	public void test() {
+		assertEquals(11,add.main1());
+	}
+
+	
+}
